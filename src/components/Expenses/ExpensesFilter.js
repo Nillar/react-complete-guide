bf1ff2ec5b0ca055a283282e38d146391ac37857
Nillar <martin.p.nikolov@gmail.com>
2022-11-28 14:27:21 +0200

    Second assignment

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,27 @@
+import './ExpensesFilter.css';
+import { useState } from 'react';
+
+const ExpensesFilter = (props) => {
+  const [enteredYear, setEnteredYear] = useState('');
+
+  const changeHandler = (event) => {
+    setEnteredYear(event.target.value);
+    props.onChangeYear(event.target.value)
+  }
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={changeHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
